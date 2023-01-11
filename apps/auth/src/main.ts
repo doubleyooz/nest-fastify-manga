1971a55e8abd1bb22031d5e6b11f8fcafa045d06
Waifu Forever <sirjoandersonwf@gmail.com>
2023-01-11 15:53:18 -0300

    chore: add auth

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,19 @@
+import { RmqService } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { RmqOptions } from '@nestjs/microservices';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const rmqService = app.get<RmqService>(RmqService);
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice<RmqOptions>(rmqService.getOptions('AUTH', true));
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT') || 5500);
 }
 bootstrap();
